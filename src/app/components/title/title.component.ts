44f299abfa4e2d29fc4350bf76785105f71f9b1c
Renan Souza <renansouza0122@gmail.com>
2023-10-25 20:54:35 -0200

    title added and stylized

--- src/app/components/title/title.component.ts
+++ src/app/components/title/title.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-title',
   templateUrl: './title.component.html',
-  styleUrls: ['./title.component.scss']
+  styleUrls: ['./title.component.scss','title.responsive.component.scss']
 })
 export class TitleComponent {
 
