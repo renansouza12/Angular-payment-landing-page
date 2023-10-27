67fee58b5f1ca8eeb0845a34ee3c0be536c274ca
RenanSouza <renansouza0122@gmail.com>
2023-10-27 16:10:11 -0300

    image size updated

--- src/app/components/phone-image/phone-image.component.ts
+++ src/app/components/phone-image/phone-image.component.ts
@@ -5,13 +5,9 @@ import { Component } from "@angular/core";
     template:`<img src="../../assets/hero-image.png" alt="">`,
     styles:[`
         img{
-            width:90%;
+            width:100%;
         }
-        @media screen and (max-width:425px){
-                img{
-                    width:100%;
-                }
-        }
+
     `]
 })
 export class PhoneImageComponent{}
